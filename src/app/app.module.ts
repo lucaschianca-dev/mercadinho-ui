ec9ff8cd2a2c431fcc95b108da8905fa9efbc5d5
lucaschianca-dev <lucaseduardochianca.dev@gmail.com>
2023-11-02 22:17:36 -0300

    projeto estilizado / adicionado home e footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
-import { MatIconModule } from '@angular/material/icon';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatTableModule } from '@angular/material/table';
+import { MatTooltipModule } from '@angular/material/tooltip';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -11,9 +12,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CadastraProdutosComponent } from './Pages/cadastra-produtos/cadastra-produtos.component';
 import { HomeComponent } from './Pages/home/home.component';
+import { FooterComponent } from './Pages/Partials/footer/footer.component';
 import { NavbarComponent } from './Pages/Partials/navbar/navbar.component';
-import { ProdutosComponent } from './Pages/produtos/produtos.component';
 import { ProdutosInativosComponent } from './Pages/produtos-inativos/produtos-inativos.component';
+import { ProdutosComponent } from './Pages/produtos/produtos.component';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -23,6 +26,7 @@ import { ProdutosInativosComponent } from './Pages/produtos-inativos/produtos-in
     CadastraProdutosComponent,
     NavbarComponent,
     ProdutosInativosComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
@@ -33,7 +37,9 @@ import { ProdutosInativosComponent } from './Pages/produtos-inativos/produtos-in
     BrowserAnimationsModule,
     MatTableModule,
     MatPaginatorModule,
-    MatIconModule
+    MatIconModule,
+    MatTooltipModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
